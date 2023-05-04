0dd913cbc07aedc8486b6d4f5b689e3bd37a6762
FrancoLDG <franco_leodg@hotmail.com>
2023-05-04 20:11:58 -0300

    casi todo completo, js html y css

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function encriptar(){
     let parrafo = document.getElementById("parrafo");
     let muñeco  = document.getElementById("muñeco");
     let textoCifrado = texto
-                        .replace(/a/gi, "ai")
+                        
                         .replace(/e/gi, "enter")
                         .replace(/i/gi, "imes")
+                        .replace(/a/gi, "ai")
                         .replace(/o/gi, "ober")
                         .replace(/u/gi, "ufat");
     
@@ -21,4 +22,30 @@ function encriptar(){
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         alert("debes ingresar un texto");
      }
+}
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco  = document.getElementById("muñeco");
+    let textoCifrado = texto
+                        
+                        .replace(/enter/gi, "e")
+                        .replace(/imes/gi, "i")
+                        .replace(/ai/gi, "a")
+                        .replace(/ober/gi, "o")
+                        .replace(/ufat/gi, "u");
+    if (texto.length !=0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "texto desencriptado con exito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/skyrim3.png";
+    }else{
+        muñeco.src = "./img/skyrim-2.png";
+        tituloMensaje.textContent = "ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("debes ingresar un texto");
+    }
+    
 }
